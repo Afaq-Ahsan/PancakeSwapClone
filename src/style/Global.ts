a6514e4012b928dcd7667fd2a3be493b4deb8c20
“Afaq <afaqahsan23@gmail.com>
2023-10-05 18:42:57 +0500

    first commit

---
+++ src/style/Global.ts
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: ${({ theme }) => theme.colors.background};
+
+    img {
+      height: auto;
+      max-width: 100%;
+    }
+  }
+
+  ul {
+    list-style: none; 
+  }
+
+  li {
+    display: flex;
+    align-items: center;
+  }
+
+  li::before {
+    content: "•";
+    color: ${({ theme }) => theme.colors.primary};
+    margin-right: 8px;
+  }
+  iframe{
+    display : none
+    
+  }
+`
+
+export default GlobalStyle
